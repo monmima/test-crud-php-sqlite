4dabc9208a6cdb4f84d48c461b59aa0907de3395
monmima <monmima@protonmail.com>
2022-11-04 21:51:32 -0400

    create test.php; update members.db

--- test.php
+++ test.php
@@ -1,3 +1,24 @@
+<form action="/test.php" method="POST">
+    <input type="text" name="name" id="name">
+    <input type="text" name="hello" id="hello">
+    <button type="submit">submit</button>
+</form>
+
 <?php
-    echo "hello";
+    if (isset($_POST["name"])) {
+
+        echo $_POST["name"] . "<br>";
+        echo $_POST["hello"] . "<br>";
+
+
+        include 'dbconfig.php';
+        
+        //query from the table that we create
+        $sql = "SELECT rowid, * FROM members";
+        $query = $db->query($sql);
+
+        print_r($query->fetchArray());
+    }
+
+
 ?>
